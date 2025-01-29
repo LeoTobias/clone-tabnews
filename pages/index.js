43d58dc9c4cbc6fb6daac76b4d241167f3dee8af
Leonardo Tobias <tobiasleonardo7@gmail.com>
2025-01-29 02:31:53 +0000

    Modifica a frase para novo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Cada dia é uma nova oportunidade para ser a melhor versão de si mesmo</h1>
+    return <h1>Cada dia é uma nova oportunidade para ser a melhor versão de si mesmo! </h1>
 }
 
 export default Home;
