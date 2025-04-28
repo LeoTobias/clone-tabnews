ceded12c62ce8ba77f4a0f104af250e077634471
Leonardo Tobias <tobiasleonardo7@gmail.com>
2025-04-28 12:04:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Cada dia é uma nova oportunidade para ser a melhor versão de si mesmo! </h1>
+  return (
+    <h1>
+      Cada dia é uma nova oportunidade para ser a melhor versão de si
+      mesmo!{" "}
+    </h1>
+  );
 }
 
 export default Home;
